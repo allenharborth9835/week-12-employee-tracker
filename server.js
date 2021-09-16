68ada059c052604a0f38218b04c98c938f4b589f
Harborth, Allen <allenharborth9835@gmail.com>
2021-09-15 22:10:50 -0500

    add update employee role function

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-
 const db = require("./db/connection");
 let inquirer = require("inquirer");
-const getTable = require("./utils/get_tables")
-let departmentChoice = [];
-let managerChoices = [];
-let roleChoices = [];
-const employees = `SELECT employees.id, employees.first_name, employees.last_name, employees.manager_id AS manager,
-                    roles.title AS role, roles.salary AS salary, departments.name AS department
+const {getTable, createDepartment, createRole, createEmployee, changeEmployeeRole} = require("./utils/routes")
+
+const employees = `SELECT employees.id, employees.first_name, employees.last_name, roles.title AS role, roles.salary AS salary,
+                    departments.name AS department, manager.first_name AS manager_first_name, manager.last_name AS manager_last_name
                     FROM employees
                     LEFT JOIN roles ON employees.role_id = roles.id
-                    LEFT JOIN departments ON roles.department_id = departments.id`
+                    LEFT JOIN departments ON roles.department_id = departments.id
+                    LEFT JOIN employees manager ON manager.id = employees.manager_id`
+
 const departments = 'SELECT * FROM departments';
+
 const roles = `SELECT roles.*,
                 departments.name AS department_assigned
                 FROM roles
                 LEFT JOIN departments ON roles.department_id = departments.id`;
-
 
 function mainMenu(){
   inquirer
@@ -34,91 +33,30 @@ function mainMenu(){
         "Update employee's role"
       ]
     })
-  .then( answer=>{
+  .then(async answer=>{
     switch(answer.options){
       case "Veiw all employees":
-        new async function(){
-          let rows = await getTable(employees);
-          console.table(rows);
-          mainMenu();
-        }
+        console.table(await getTable(employees));
         break;
       case "Veiw departments":
-        getTable(departments);
-        mainMenu();
+        console.table(await getTable(departments));
         break;
       case "Veiw roles":
-        getTable(roles)
-        mainMenu();
+        console.table(await getTable(roles));
         break;
       case "Add Employee":
+        await createEmployee();
         break;
       case "Add department":
-        createDepartment();
+        await createDepartment();
         break;
       case "Add role":
-        createRole();
+        await createRole();
         break;
       case "Update employee's role":
+        await changeEmployeeRole();
         break;
     };
-  });
-};
-
-
-
-function getDepts(){
-  db.query("SELECT * FROM departments",(err, data)=>{
-    if(err)throw err;
-    for(i=0; i< data.length; i++){
-      departmentChoice.push(data[i].id+"-"+data[i].name)
-    }
-  })
-}
-
-function createRole(){
-  getDepts()
-  inquirer.prompt([
-    {
-      name: "role",
-      type:"input",
-      message:"enter name of role:"
-    },
-    {
-      name: "dept",
-      type: "list",
-      message: "what is the roles department?",
-      choices: departmentChoice
-    },
-    {
-      name:"salary",
-      type:"number",
-      message:"enter salary"
-    },
-    {
-      name:"desc",
-      type:"input",
-      message:"add short description"
-    },
-  ]).then(answer=>{
-    var getDeptId = answer.dept.split("-")
-    db.query(`INSERT INTO roles(title, salary, department_id, description)
-              VALUES('${answer.role}', '${answer.salary}', '${getDeptId[0]}', '${answer.desc}')`,
-              err=>{
-                if(err) throw err
-                console.log(`added ${answer.role} to roles`)  
-    });
-    mainMenu();
-  });
-};
-function createDepartment(){
-  inquirer.prompt({
-    name:"dept",
-    type:"input",
-    message:"Enter new department:"
-  }).then(answer=>{
-    db.query(`INSERT INTO departments(name) VALUES('${answer.dept}')`);
-    console.log(`element created ${answer.dept}`)
     mainMenu();
   });
 };
---
+++ utils/routes.js
@@ -0,0 +1,174 @@
+const db = require("../db/connection");
+let inquirer = require("inquirer");
+
+let departmentChoice = [];
+let employeeChoice = [];
+let roleChoice = [];
+
+function getTable(sqlArgs){
+  return new Promise((Resolve, Reject)=>{
+    db.query(sqlArgs, (err, Table, feilds)=>{
+      Resolve(Table)
+    })
+  })
+};
+
+function createDepartment(){
+  return new Promise((res, rej)=>{
+    inquirer.prompt({
+      name:"dept",
+      type:"input",
+      message:"Enter new department:"
+    }).then(answer=>{
+      db.query(`INSERT INTO departments(name) VALUES('${answer.dept}')`);
+      res(console.log(`element created ${answer.dept}`))
+    })
+  })
+}
+
+function getDepts(){
+  db.query("SELECT * FROM departments",(err, data)=>{
+    if(err)throw err;
+    for(i=0; i< data.length; i++){
+      departmentChoice.push(data[i].id+"-"+data[i].name)
+    }
+  })
+}
+
+function getRoles(){
+  db.query("SELECT * FROM roles",(err, data)=>{
+    if(err)throw err;
+    for(i=0; i< data.length; i++){
+      roleChoice.push(data[i].id+"-"+data[i].title)
+    }
+  })
+}
+
+function getEmployees(){
+  db.query("SELECT * FROM employees",(err, data)=>{
+    if(err)throw err;
+    for(i=0; i< data.length; i++){
+      employeeChoice.push(data[i].id+"-"+data[i].first_name+" "+data[i].last_name)
+    }
+  })
+}
+
+function createRole(){
+  return new Promise((res,req)=>{
+    getDepts()
+    console.log(departmentChoice)
+    inquirer.prompt([
+      {
+        name: "role",
+        type:"input",
+        message:"enter name of role:"
+      },
+      {
+        name: "dept",
+        type: "list",
+        message: "what is the roles department?",
+        choices: departmentChoice
+      },
+      {
+        name:"salary",
+        type:"number",
+        message:"enter salary"
+      },
+      {
+        name:"desc",
+        type:"input",
+        message:"add short description"
+      },
+    ]).then(answer=>{
+      let getDeptId = answer.dept.split("-")
+      db.query(`INSERT INTO roles(title, salary, department_id, description)
+                VALUES('${answer.role}', '${answer.salary}', '${getDeptId[0]}', '${answer.desc}')`,
+                err=>{
+                  if(err) throw err
+                  res(console.log(`added ${answer.role} to roles`))
+      });
+    });
+  });
+};
+
+function createEmployee(){
+  return new Promise((res, req)=>{
+    getRoles();
+    getEmployees();
+    console.log(employeeChoice);
+    inquirer.prompt([
+      {
+        name: "first_name",
+        type:"input",
+        message:"enter the first name of Employee:"
+      },
+      {
+        name: "last_name",
+        type:"input",
+        message:"enter the last name of Employee:"
+      },
+      {
+        name: "role",
+        type: "list",
+        message: "what is the employee's role?",
+        choices: roleChoice
+      },
+      {
+        name:"manager",
+        type:"list",
+        message:"who is the employee's manager?",
+        choices: employeeChoice
+      }
+    ]).then(answer=>{
+      let getRole = answer.role.split("-")
+      let getEmployee = answer.manager.split("-")
+      db.query(`INSERT INTO employees(first_name, last_name, role_id, manager_id)
+                VALUES('${answer.first_name}','${answer.last_name}','${getRole[0]}', '${getEmployee[0]}')`,
+                err=>{
+                  if(err) throw err
+                  res(console.log(`added ${answer.last_name}, ${answer.first_name} to employees`))
+                })
+    })
+  })
+}
+
+function changeEmployeeRole(){
+  return new Promise((res, req)=>{
+    getEmployees();
+    getRoles();
+    console.log(employeeChoice);
+    inquirer.prompt([
+      {
+        type: "confirm",
+        name: "confirm",
+        message: "are sure you wish to change an employees role"
+      },
+      {
+        type: "list",
+        name: "employee",
+        message:"choose the Employee you wish to change",
+        choices: employeeChoice
+      },
+      {
+        type: "list",
+        name: "role",
+        message: "what is the employee's new role?",
+        choices: roleChoice
+      }
+    ]).then(answer=>{
+      if(answer.confirm){
+        let getRole = answer.role.split("-")
+        let getEmployee = answer.employee.split("-")
+        db.query(`UPDATE employees SET role_id = ${getRole[0]}
+                  WHERE id = ${getEmployee[0]}`,
+                  err=>{
+                    if(err) throw err
+                    res(console.log(`changed ${getEmployee[1]}'s to ${getRole[1]}`))
+                })
+      }else{
+      }
+    })
+  })
+}
+
+module.exports = {getTable, createDepartment, createRole, createEmployee, changeEmployeeRole};
